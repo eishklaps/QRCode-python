90ae4d895454c9b9f0c1211d8bcba95499591d51
eishklaps <90578188+eishklaps@users.noreply.github.com>
2022-03-09 12:13:35 +0300

    Update read_qr.py

--- read_qr.py
+++ read_qr.py
@@ -2,6 +2,6 @@ import cv2
 
 d = cv2.QRCodeDetector()
 
-val, points, qrcode = d.detectAndDecode(cv2.imread("img/qr_details.png"))
+val, points, qrcode = d.detectAndDecode(cv2.imread("https://github.com/LuNiZz/davet/blob/main/qrcode.png?raw=true"))
 
 print(val)
